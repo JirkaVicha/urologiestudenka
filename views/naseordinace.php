f629d1e57d88d7d602ceb6596dda03e44ca2956c
JirkaVicha <88149618+JirkaVicha@users.noreply.github.com>
2023-04-07 15:16:37 +0200

    Update naseordinace.php

--- views/naseordinace.php
+++ views/naseordinace.php
@@ -3,10 +3,10 @@
 $content = '
                 <h2>Naše ordinace</h2>
                 <p>
-                    <img src="../img/ordinace1.jpg" alt="foto ordinace" width="500">
+                    <img src="https://urologiestudenka.cz/img/ordinace1.jpg" alt="foto ordinace" width="500">
                 </p>
                 <p>
-                    <img src="../img/ordinace2.jpg" alt="foto ordinace" width="500">
+                    <img src="https://urologiestudenka.cz/img/ordinace2.jpg" alt="foto ordinace" width="500">
                 </p>
 ';
 
