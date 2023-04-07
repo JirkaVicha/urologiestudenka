4033b9bed0300825e73536d51de8f0a5f2b1bb03
JirkaVicha <88149618+JirkaVicha@users.noreply.github.com>
2023-04-07 15:15:57 +0200

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -6,18 +6,18 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Urologická ambulance - Studénka</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="../src/main.css">
+  <link rel="stylesheet" href="https://urologiestudenka.cz/src/main.css">
 </head>
 <body>
 
 <div class="header-container">
  <header class="wrapper clearfix">
-  <img src="../img/ico_main.png" alt="Urologie MUDr. Sobková Lenka" height="74" width="74" style="float: left; padding: 10px 15px 0 0;"/>
+  <img src="https://urologiestudenka.cz/img/ico_main.png" alt="Urologie MUDr. Sobková Lenka" height="74" width="74" style="float: left; padding: 10px 15px 0 0;"/>
    <h1 class="title">Urologická ambulance<br /><span style="font-size: 0.6em;">MUDr. Sobková Lenka</span></h1>
     <nav>
      <ul>
-      <li><a href="home.php">Hlavni stranka</a></li>
-      <li><a href="naseordinace.php">Naše ordinace</a></li>
+      <li><a href="https://urologiestudenka.cz/views/home.php">Hlavni stranka</a></li>
+      <li><a href="https://urologiestudenka.cz/views/naseordinace.php">Naše ordinace</a></li>
      </ul>
     </nav>
  </header>
@@ -60,7 +60,7 @@
     <strong>Ke každému vyšetření je nutné se předem TELEFONICKY objednat!</strong>
     <h2><i class="fa-solid fa-square-phone"></i> 605 880 937</h2>
     <h3><i class="fa-solid fa-envelope"></i> <a href="mailto:urol.studenka@seznam.cz">urol.studenka@seznam.cz</a></h3>
-    <h3><i class="fa-solid fa-map"></i> <a href="kdenasnajdete.php">Jak se k nám dostat?</a></h3>
+    <h3><i class="fa-solid fa-map"></i> <a href="https://urologiestudenka.cz/views/kdenasnajdete.php">Jak se k nám dostat?</a></h3>
 </aside>
 
     </div> <!-- #main -->
