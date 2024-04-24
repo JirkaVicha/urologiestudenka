30965a114953187da6978d78ff5e2fde6b5c2c68
JirkaVicha <88149618+JirkaVicha@users.noreply.github.com>
2024-04-24 07:40:59 +0200

    Update db.php

--- model/db.php
+++ model/db.php
@@ -1,10 +1,10 @@
 <?php
 
 class Database {
-  private static $db_server = 'localhost'; //sql4.webzdarma.cz
-  private static $username = 'root'; // urologiestud8658
-  private static $password = ''; // Hsls.3327
-  private static $db_name = 'urology'; // urologiestud8658
+  private static $db_server = 'localhost'; //sql........z
+  private static $username = 'root'; // urol.......8
+  private static $password = ''; // H.....7
+  private static $db_name = 'urology'; // uro........8
 
   public static function connect() {
     $db = new mysqli(self::$db_server, self::$username, self::$password, self::$db_name);
